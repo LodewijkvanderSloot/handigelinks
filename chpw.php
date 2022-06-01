fdcb741b09a2599dcf43b9721018fd5c6662badf
LodewijkvanderSloot <lodewijk@hebbium.nl>
2022-06-01 20:58:39 +0200

    Update chpw.php

--- chpw.php
+++ chpw.php
@@ -8,6 +8,31 @@ include "dbconn.php";
 include "header.php";
 $persoonid = "";
 $persoonid = $_SESSION["id"];
+$loginnamevalue = "";
+$passwordvalue = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $oldpassword = test_input($_POST["oldpw"]);
+    $newpassword1 = test_input($_POST["newpw1"]);
+    $newpassword2 = test_input($_POST["wachtwoord"]);
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+//test if new passwords match
+if ($newpassword1 == $newpassword2 {
+    //test if new password is different
+    if (newpassword1 == $oldpassword) {
+        $foutje = "Wachtwoord mag niet hetzelfde zijn."
+    } else {
+        //Doe dingen met wijzigen van het wachtwoord. 
+} else {
+   $foutje = "Nieuwe wachtwoorden zijn niet hetzelfde."
+}
 ?>
 
     <body>
@@ -15,7 +40,7 @@ $persoonid = $_SESSION["id"];
             <li class="actief"><a href=chpw.php>Wijzig wachtwoord</a></li>
             <li class="menu"><a href="index.php">Handige Links</a></li>
         </ul>
-        <form method="post" action="setnewpw.php" id="newpwform">
+        <form method="post" action="chpw.php" id="newpwform">
             <table>
                 <tr>
                     <td><label for="oldpw">Oude wachtwoord:</label></td>
@@ -31,6 +56,9 @@ $persoonid = $_SESSION["id"];
                 </tr>
                 <tr>
                     <td colspan="2" style="text-align:right"><input type="submit"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><span class="help_block"><?php echo $foutje; ?></span></td>
                 </tr>
             </table>
             

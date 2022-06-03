f9bd57693a47658b58b6987bac931c826f98a6a7
LodewijkvanderSloot <lodewijk@hebbium.nl>
2022-06-03 11:05:49 +0200

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+include "dbconn.php";
+include "header.php";
+$PersoonID = $_SESSION["id"]
+?>
+
+    <body>
+        <ul class="menu">
+            <li class="actief"><a href=index.php>Handige Links</a></li>
+            <li class="menu"><a href="logoff.php">Afmelden</a></li>
+            <li class="menu"><a href="chpw.php">Wachtwoord wijzigen</a></li>
+            <li class="menu"><a href="newcategory.php">Nieuwe Categorie</a></li>
+            <li class="menu"><a href="newlink.php">Nieuwe link</a></li>
+        </ul>
+      Hier moeten dingen komen als Kleur kiezen, Admin maken en taal kiezen. 
+    </body>
+</html>
+<?php
+$ConnHandigelinksDB -> close();
+?>

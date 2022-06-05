d3afc26550bbcd0e12a3c87c56ad6131db5cafa1
Lodewijk <lodewijk@hebbium.nl>
2022-06-05 16:02:38 +0200

    lang for index

--- index.php
+++ index.php
@@ -6,22 +6,23 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 include "dbconn.php";
 include "header.php";
+include "lang.php";
 $PersoonID = $_SESSION["id"]
 ?>
 
     <body>
         <ul class="menu">
-            <li class="actief"><a href=index.php>Handige Links</a></li>
-            <li class="menu"><a href="logoff.php">Afmelden</a></li>
-            <li class="menu"><a href="newcategory.php">Nieuwe Categorie</a></li>
-            <li class="menu"><a href="newlink.php">Nieuwe link</a></li>
+            <li class="actief"><a href=index.php><?php echo $hanlinkvar; ?></a></li>
+            <li class="menu"><a href="logoff.php"><?php echo $logoffvar; ?></a></li>
+            <li class="menu"><a href="newcategory.php"><?php echo $newcatvar; ?></a></li>
+            <li class="menu"><a href="newlink.php"><?php echo $newlinkvar; ?></a></li>
             <?php
             $sql = "SELECT IsAdmin FROM tblPersonen WHERE PersoonID = '$PersoonID'";
             if ($adminresult = $ConnHandigelinksDB -> query($sql)) {
                 while ($admin = $adminresult -> fetch_object()) {
                     if ($admin->IsAdmin == 1) {
                         ?>
-                        <li class="menu"><a href="settings.php">Instellingen</a></li>
+                        <li class="menu"><a href="settings.php"><?php echo $settingsvar; ?></a></li>
                         <?php
                     }
                 }
@@ -43,25 +44,19 @@ $PersoonID = $_SESSION["id"]
                         if ($Categorie->PersoonID == $PersoonID) { ?>
                             <td width="25%"><h1><?php echo($Categorie->Categorienaam); ?></h1></td>
                         <?php } else { ?>
-                            <td width="25%"><h1><?php echo($Categorie->Categorienaam); ?> (Publiek)</h1></td>
+                            <td width="25%"><h1><?php echo($Categorie->Categorienaam); ?> <?php echo $publicvar; ?></h1></td>
                         <?php } ?>
-                        <td style="padding-right:15px;text-align:right" width="75%"><a href="editcategory.php?categoryid=<?php echo($Categorie->CategorieID); ?>" class="small"><b>Bewerken<b></a>
+                        <td style="padding-right:15px;text-align:right" width="75%"><a href="editcategory.php?categoryid=<?php echo($Categorie->CategorieID); ?>" class="small"><b><?php echo $editvar; ?><b></a>
                         <?php
                         if ($linkresult->num_rows == 0) { 
                             ?>
-                            <a href="deletecategory.php?categoryid=<?php echo($Categorie->CategorieID); ?>" class="small"><b>Verwijderen<b></a>
+                            <a href="deletecategory.php?categoryid=<?php echo($Categorie->CategorieID); ?>" class="small"><b><?php echo $deletevar; ?><b></a>
                             <?php
                         }
                         ?>
                     </tr>
                 </table>
                 <hr>
-                <?php
-              //printf("<h1>%s<small style='text-align:right;text-decoration:none;font-size:8pt'>test</small></h1><hr>", $Categorie->Categorienaam);
-              
-              //echo $sql;
-              ?>
-
               <table width="100%">
               <?php
               
@@ -70,7 +65,7 @@ $PersoonID = $_SESSION["id"]
                       <tr>
                           <td><a href="<?php echo($link->Link); ?>" target="_blank"><img src="<?php echo($link->Favicon); ?>" style="width:16px;height:16px"></a></td>
                           <td width="25%"><a href="<?php echo($link->Link); ?>" target="_blank"><?php echo($link->Linknaam); ?></a></td>
-                          <td style="padding-right:15px;text-align:right" width="75%"><a href="editlink.php?linkid=<?php echo($link->LinkID); ?>" class="small">Bewerken</a><a href="deletelink.php?linkid=<?php echo($link->LinkID); ?>" class="small">Verwijderen</a></td>
+                          <td style="padding-right:15px;text-align:right" width="75%"><a href="editlink.php?linkid=<?php echo($link->LinkID); ?>" class="small"><?php echo $editvar; ?></a><a href="deletelink.php?linkid=<?php echo($link->LinkID); ?>" class="small"><?php echo $deletevar; ?></a></td>
                       </tr>
                       <?php
                   }
--- lang.php
+++ lang.php
@@ -1,11 +1,19 @@
 <?php
-$Lang = "EN";
+$Lang = "NL";
 If ($Lang == "NL"){ 
     $registervar = "Registreren";
     $loginvar = "Aanmelden";
     $namelbl = "Naam:";
     $passwordlbl = "Wachtwoord:";
     $errormsg1 = "Gebruikersaccount of wachtwoord klopt niet!";
+    $hanlinkvar = "Handige links";
+    $logoffvar = "Afmelden";
+    $newcatvar = "Nieuwe categorie";
+    $newlinkvar = "Nieuwe link";
+    $settingsvar = "Instellingen";
+    $publicvar = "(Publiek)";
+    $editvar = "Bewerken";
+    $deletevar = "Verwijderen";
 }
 If ($Lang == "EN"){ 
     $registervar = "Register";
@@ -13,5 +21,13 @@ If ($Lang == "EN"){
     $namelbl = "Name:";
     $passwordlbl = "Password:";
     $errormsg1 = "User account or password are incorrect!";
+    $hanlinkvar = "Handy links";
+    $logoffvar = "Logoff";
+    $newcatvar = "New category";
+    $newlinkvar = "New link";
+    $settingsvar = "Settings";
+    $publicvar = "(Public)";
+    $editvar = "Edit";
+    $deletevar = "Delete";
 }
 ?>
